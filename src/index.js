1b30ca115edcdc1183a376f2e99a4a1527118675
MissHalle <100787203+MissHalle@users.noreply.github.com>
2025-10-24 17:11:03 -0400

    fixed the i.e

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="waiting">Generating a poem about ${instructionsInput.value}</div>`;
   axios.get(apiURL).then(displayPoem);
   // build an API URl, call the API, display generated poem
 }
